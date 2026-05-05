6ac8328bb27eac9461876d10a24739e13444ced4
TheThirdPlayerFromTheLeft <souleater142356@gmail.com>
2026-05-04 17:45:05 -0700

    lab 13 files

---
+++ lab13script.js
@@ -0,0 +1,54 @@
+const profiles = [
+  {
+    name: "Kevin Alvarez",
+    pic: "shinji.jpg",
+    desc: "Hey! My name is Kevin Alvarez, I am a transfer student from Cerritos College, and I am currently in my final semester here at CSULB. ",
+    classes: ["CECS 427", "CECS 435", "CECS 455", "CECS 470"],
+    favs: [
+      "Neon Genesis Evangelion",
+      "A Knight of Seven Kingdoms",
+      "Shin Godzilla",
+    ],
+  },
+  {
+    name: "James Curran",
+    pic: "Red_Panda_Plush.jpg",
+    desc: "My name is James Curran, and I graduate <b>this semester!</b> I like to play <b>tabletop rpgs</b> and board games with my friends",
+    classes: ["CECS 470", "CECS 474"],
+    favs: ["Star Wars IV", "Back to the Future", "Klaus (2019)"],
+  },
+  {
+    name: "Anthony Theng",
+    pic: "Simon.jpg",
+    desc: "Hey, I'm Anthony Theng and this is my final semester at CSULB. I enjoy watching anime and movies.",
+    classes: ["CECS 470", "CECS 475", "REC 340"],
+    favs: [
+      "Cyberpunk Edgerunners",
+      "Gurren Lagann",
+      "Puss in Boots: The Last Wish",
+    ],
+  },
+];
+
+let current_index = 0;
+
+function updateProfile() {
+  const p = profiles[current_index];
+  document.getElementById("name").textContent = p.name;
+  document.getElementById("pic").src = p.pic;
+  document.getElementById("desc").innerHTML = p.desc;
+
+  const class_list = document.getElementById("classes");
+  class_list.innerHTML = p.classes.map((item) => `<li>${item}</li>`).join("");
+
+  const favs_list = document.getElementById("favorites");
+  favs_list.innerHTML = p.favs.map((item) => `<li>${item}</li>`).join("");
+}
+
+document.getElementById("next-btn").addEventListener("click", () => {
+  // Loops back to 0
+  current_index = (current_index + 1) % profiles.length;
+  updateProfile();
+});
+
+updateProfile();
